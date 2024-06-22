ab9008489145a57f74ca1fa579e39e0dbfe11891
skandar123 <35923180+skandar123@users.noreply.github.com>
2024-06-22 19:26:06 +0530

    Update table_query.php

--- tables/table_query.php
+++ tables/table_query.php
@@ -7,7 +7,7 @@ $conn5 = pg_connect("$host $port dbname=$db1 $credentials");
 if(!$conn5) {
     echo "Error : Unable to open database\n";
  }
-$conn6 = pg_connect("$host $port dbname=$db2 $credentials");
+$conn6 = pg_connect("$host_other $port_other dbname=$db2 $credentials_other");
 if(!$conn6) {
     echo "Error : Unable to open database\n";
  }
