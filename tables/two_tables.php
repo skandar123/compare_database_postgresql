4b9c805a3df9cb9814ce82b16d92eeac4370c70d
skandar123 <35923180+skandar123@users.noreply.github.com>
2024-06-22 19:29:33 +0530

    Update two_tables.php

--- tables/two_tables.php
+++ tables/two_tables.php
@@ -1,11 +1,12 @@
 <?php
 include("config.php");
+include("config2.php");
 include("table_query.php");
 include("common_functions.php");
 ?>
 <!DOCKTYPE html>
 <head>
-<title>Compare Tables</title>
+<title>Compare Databases</title>
 <link rel="stylesheet" href="style_compare_tables.css">
 </head>
 
@@ -14,11 +15,10 @@ include("common_functions.php");
 <div>
 <p>
 <a href="select_db.php">Select Databases</a>
-> Select Tables > Two Tables
+> Select Schemas > Select Tables > Two Tables
 </p>
 </div>
 <?php
-
 $table1 = $_POST['table1'];
 $table2 = $_POST['table2'];
 
@@ -31,6 +31,24 @@ $diff1=array_diff($columns1, $columns2);
 $diff2=array_diff($columns2, $columns1);
 ?>
 
+<div class="table-container">
+<table>
+    <tr><th colspan="4">Data Selected</th><tr>
+    <tr>
+        <td><b>Database1:</b></td><td><?php echo $db1; ?></td>
+        <td><b>Database2:</b></td><td><?php echo $db2; ?></td>
+    </tr>
+    <tr>
+        <td><b>Schema1:</b></td><td><?php echo $s1; ?></td>
+        <td><b>Schema2:</b></td><td><?php echo $s2; ?></td>
+    </tr>
+    <tr>
+        <td><b>Table1:</b></td><td><?php echo $table1; ?></td>
+        <td><b>Table2:</b></td><td><?php echo $table2; ?></td>
+    </tr>
+</table>
+</div>
+
     <div class="left">
     <h3>Matching Columns</h3>
 
@@ -39,7 +57,8 @@ $diff2=array_diff($columns2, $columns1);
 
     <div class="grid-container">
     <div class="grid-item tab-heading"></div>
-    <div class="grid-item tab-heading"><b><?php echo $table1; ?> of <?php echo $db1; ?></b></div>
+    <div class="grid-item tab-heading">
+        <b>Database:</b> <?php echo $db1; ?></div>
     
         <div class="grid-item green"><b>Column</b></div>
         <div class="grid-item green"><b>DataType</b></div>
@@ -55,7 +74,8 @@ $diff2=array_diff($columns2, $columns1);
     <div class="tab">
     
     <div class="grid-container-single">
-    <div class="grid-item tab-heading"><b><?php echo $table2; ?> of <?php echo $db2; ?></b></div>
+    <div class="grid-item tab-heading">
+        <b>Database:</b> <?php echo $db2; ?></div>
     
         <div class="grid-item green"><b>DataType</b></div>
         <?php
@@ -75,7 +95,8 @@ $diff2=array_diff($columns2, $columns1);
     <div class="tab">
     
     <div class="grid-container">
-    <div class="grid-item span tab-heading"><b><?php echo $table1; ?> of <?php echo $db1; ?></b></div>
+    <div class="grid-item span tab-heading">
+        <b>Database:</b> <?php echo $db1; ?></div>
    
         <div class="grid-item green"><b>Column</b></div>
         <div class="grid-item green"><b>DataType</b></div>
@@ -90,7 +111,8 @@ $diff2=array_diff($columns2, $columns1);
     <div class="tab space">
     
     <div class="grid-container">
-    <div class="grid-item span tab-heading"><b><?php echo $table2; ?> of <?php echo $db2; ?></b></div>
+    <div class="grid-item span tab-heading">
+        <b>Database:</b> <?php echo $db2; ?></div>
 
         <div class="grid-item green"><b>Column</b></div>
         <div class="grid-item green"><b>DataType</b></div>
